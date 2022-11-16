ab727ce5baec317afa1622d921a14d508cc79d99
ekaras <key.cpp@gmail.com>
2022-11-17 00:01:41 +0300

    second controller

---
+++ PassDateProject/SecondViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SecondViewController.swift
+//  PassDateProject
+//
+//  Created by Евгений Карась on 16.11.22.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+ 
+    @IBOutlet weak var label: UILabel!
+    
+    @IBAction func goBackTapped(_ sender: UIButton) {
+    }
+    
+}
