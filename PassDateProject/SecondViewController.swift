ebb61711910a2eb2faeed94beb60dcb6c1135a88
ekaras <key.cpp@gmail.com>
2022-11-17 01:00:18 +0300

    go to second viev from vievcontroller

--- PassDateProject/SecondViewController.swift
+++ PassDateProject/SecondViewController.swift
@@ -8,8 +8,18 @@
 import UIKit
 
 class SecondViewController: UIViewController {
- 
+    
+    var login: String?
+    
     @IBOutlet weak var label: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        guard let login = self.login else { return }
+        label.text = "Hello, \(login)"
+ 
+    }
     
     @IBAction func goBackTapped(_ sender: UIButton) {
     }
--- PassDateProject/ViewController.swift
+++ PassDateProject/ViewController.swift
@@ -14,7 +14,18 @@ class ViewController: UIViewController {
     @IBOutlet weak var passwordTF: UITextField!
     
     @IBAction func loginTapped(_ sender: UIButton) {
+
+        performSegue(withIdentifier: "detailSegue", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let dvc = segue.destination as? SecondViewController else { return }
+        dvc.login = loginTF.text
         
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
     
     
